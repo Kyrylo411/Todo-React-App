c0b5b98c0500c193083bda317ac8d72e3f3186db
Kyrylo <kirillomelcenko411@gmail.com>
2021-08-18 12:13:50 +0300

    todo_react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+	return (
+		<div>
+			<h2>Welcome to React App</h2>
+			<h3>Date : {new Date().toDateString()}</h3>
+
+			<h3> Something from me start now!!!! </h3>
+		</div>
+	);
+}
+
+export default App;
