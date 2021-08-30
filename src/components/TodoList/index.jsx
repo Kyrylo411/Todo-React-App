67a4131031fd0430c441beea796cc764237b105c
Kyrylo <kirillomelcenko411@gmail.com>
2021-08-30 15:30:53 +0300

    Class TodoList changed to function

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 import Header from './Header';
@@ -7,102 +7,77 @@ import Footer from './Footer';
 import UnderLines from './UnderLines';
 import './TodoList.scss';
 
-class TodoList extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			todoItemList: [],
-			inputValue: '',
-			activeFilter: 'All',
-		};
+function TodoList() {
+	const [todoItemList, setTodoItemList] = useState([]);
+	const [inputValue, setInputValue] = useState('');
+	const [activeFilter, setActiveFilter] = useState('All');
 
-		this.changeInputValue = this.changeInputValue.bind(this);
-		this.addListItem = this.addListItem.bind(this);
-		this.changeItemCheck = this.changeItemCheck.bind(this);
-		this.deleteItem = this.deleteItem.bind(this);
-		this.toggleAllItems = this.toggleAllItems.bind(this);
-		this.deleteCompletedItems = this.deleteCompletedItems.bind(this);
-		this.setActiveFilter = this.setActiveFilter.bind(this);
-		this.changeItemValue = this.changeItemValue.bind(this);
-	}
-	setActiveFilter(textValue) {
-		this.setState({
-			activeFilter: textValue,
-		});
-	}
-	deleteCompletedItems() {
+	const changeActiveFilter = (textValue) => {
+		setActiveFilter(textValue);
+	};
+	const deleteCompletedItems = () => {
 		const deleteCompletedItems = async () => {
 			try {
-				const checkeditems = this.state.todoItemList.filter(
-					(item) => item.done === true,
-				);
+				const checkeditems = todoItemList.filter((item) => item.done === true);
 				await axios.delete('http://localhost:5000/todo', checkeditems);
-				this.setState({
-					todoItemList: this.state.todoItemList.filter(
-						(item) => item.done === false,
-					),
-				});
+				setTodoItemList(todoItemList.filter((item) => item.done === false));
 			} catch (e) {
 				console.log(e);
 			}
 		};
 		deleteCompletedItems();
-	}
-	toggleAllItems(isAllItemsChecked) {
+	};
+	const toggleAllItems = (isAllItemsChecked) => {
 		const updateAllItemsCheck = async () => {
 			try {
 				await axios.put(`http://localhost:5000/todo/${isAllItemsChecked}`);
-				this.setState({
-					todoItemList: this.state.todoItemList.map((item) =>
+				setTodoItemList(
+					todoItemList.map((item) =>
 						isAllItemsChecked
 							? { ...item, done: false }
 							: { ...item, done: true },
 					),
-				});
+				);
 			} catch (e) {
 				console.log(e);
 			}
 		};
 		updateAllItemsCheck();
-	}
-	changeItemCheck(id, isChecked) {
+	};
+	const changeItemCheck = (id, isChecked) => {
 		const updateItemCheck = async () => {
 			try {
 				const res = await axios.put('http://localhost:5000/todo', {
 					_id: id,
 					done: isChecked,
 				});
-				this.setState({
-					todoItemList: this.state.todoItemList.map((item) =>
+				setTodoItemList(
+					todoItemList.map((item) =>
 						item._id === res.data._id ? { ...item, done: isChecked } : item,
 					),
-				});
+				);
 			} catch (e) {
 				console.log(e);
 			}
 		};
 		updateItemCheck();
-	}
-	deleteItem(id) {
+	};
+	const deleteItem = (id) => {
 		const deleteItem = async () => {
 			try {
 				await axios.delete(`http://localhost:5000/todo/${id}`);
-				this.setState({
-					todoItemList: this.state.todoItemList.filter(
-						(item) => item._id !== id,
-					),
-				});
+				setTodoItemList(todoItemList.filter((item) => item._id !== id));
 			} catch (e) {
 				console.log(e);
 			}
 		};
 
 		deleteItem();
-	}
-	changeInputValue(textValue) {
-		this.setState({ inputValue: textValue });
-	}
-	addListItem(inputValue) {
+	};
+	const changeInputValue = (textValue) => {
+		setInputValue(textValue);
+	};
+	const addListItem = (inputValue) => {
 		const postListItem = async () => {
 			try {
 				if (inputValue.trim()) {
@@ -110,17 +85,15 @@ class TodoList extends Component {
 						value: inputValue,
 						done: false,
 					});
-					this.setState({
-						todoItemList: [
-							...this.state.todoItemList,
-							{
-								value: inputValue,
-								_id: res.data._id,
-								done: false,
-							},
-						],
-						inputValue: '',
-					});
+					setTodoItemList([
+						...todoItemList,
+						{
+							value: inputValue,
+							_id: res.data._id,
+							done: false,
+						},
+					]);
+					setInputValue('');
 				}
 			} catch (e) {
 				console.log(e);
@@ -128,8 +101,8 @@ class TodoList extends Component {
 		};
 
 		postListItem();
-	}
-	changeItemValue(inputValue, id) {
+	};
+	const changeItemValue = (inputValue, id) => {
 		const updateInputValue = async () => {
 			try {
 				if (inputValue.trim()) {
@@ -137,11 +110,11 @@ class TodoList extends Component {
 						_id: id,
 						value: inputValue,
 					});
-					this.setState({
-						todoItemList: this.state.todoItemList.map((item) => {
+					setTodoItemList(
+						todoItemList.map((item) => {
 							return item._id === id ? { ...item, value: inputValue } : item;
 						}),
-					});
+					);
 				}
 			} catch (e) {
 				console.log(e);
@@ -149,51 +122,48 @@ class TodoList extends Component {
 		};
 
 		updateInputValue();
-	}
-	componentDidMount() {
+	};
+
+	useEffect(() => {
 		const getItemsToRender = async () => {
 			try {
 				const res = await axios.get('http://localhost:5000/todo');
-				this.setState({
-					todoItemList: res.data,
-				});
+				setTodoItemList(res.data);
 			} catch (e) {
 				console.log(e);
 			}
 		};
 		getItemsToRender();
-	}
+	}, []);
 
-	render() {
-		return (
-			<>
-				<h1 className="todos">todos</h1>
-				<div className="todoWrapper">
-					<Header
-						toggleAllItems={this.toggleAllItems}
-						changeInputValue={this.changeInputValue}
-						addListItem={this.addListItem}
-						value={this.state.inputValue}
-						todoItemList={this.state.todoItemList}
-					/>
-					<List
-						activeFilter={this.state.activeFilter}
-						todoItemList={this.state.todoItemList}
-						changeItemCheck={this.changeItemCheck}
-						deleteItem={this.deleteItem}
-						changeItemValue={this.changeItemValue}
-					/>
-					<Footer
-						deleteCompletedItems={this.deleteCompletedItems}
-						todoItemList={this.state.todoItemList}
-						setActiveFilter={this.setActiveFilter}
-						activeFilter={this.state.activeFilter}
-					/>
-				</div>
-				<UnderLines visible={this.state.todoItemList.length} />
-			</>
-		);
-	}
+	return (
+		<>
+			<h1 className="todos">todos</h1>
+			<div className="todoWrapper">
+				<Header
+					toggleAllItems={toggleAllItems}
+					changeInputValue={changeInputValue}
+					addListItem={addListItem}
+					value={inputValue}
+					todoItemList={todoItemList}
+				/>
+				<List
+					activeFilter={activeFilter}
+					todoItemList={todoItemList}
+					changeItemCheck={changeItemCheck}
+					deleteItem={deleteItem}
+					changeItemValue={changeItemValue}
+				/>
+				<Footer
+					deleteCompletedItems={deleteCompletedItems}
+					todoItemList={todoItemList}
+					setActiveFilter={changeActiveFilter}
+					activeFilter={activeFilter}
+				/>
+			</div>
+			<UnderLines visible={todoItemList.length} />
+		</>
+	);
 }
 
 export default TodoList;
