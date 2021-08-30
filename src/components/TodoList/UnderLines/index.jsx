977f170a57257448b19b518acfbdc4ab208daaa5
Kyrylo <kirillomelcenko411@gmail.com>
2021-08-30 15:14:46 +0300

    Class UnderLines changed to function

--- src/components/TodoList/UnderLines/index.jsx
+++ src/components/TodoList/UnderLines/index.jsx
@@ -1,15 +1,12 @@
-import React, { Component } from 'react';
-
+import React from 'react';
 import './UnderLines.scss';
 
-class UnderLines extends Component {
-	render() {
-		return this.props.visible ? (
-			<div className="underLines">
-				<div className="firstLine" />
-				<div className="secondLine" />
-			</div>
-		) : null;
-	}
+function UnderLines({ visible }) {
+	return visible ? (
+		<div className="underLines">
+			<div className="firstLine" />
+			<div className="secondLine" />
+		</div>
+	) : null;
 }
 export default UnderLines;
