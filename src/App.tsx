5dfdd763cbfed2a4fcb287b48ce485362ad190ed
Kyrylo <kirillomelcenko411@gmail.com>
2021-09-13 18:18:03 +0300

    App clean

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,8 @@ import { Box } from '@material-ui/core';
 import { TodoListPage, AuthPage, LoginPage } from './pages';
 import ProtectedRoute from './components/PrivateRoute';
 import NavBar from './components/NavBar';
-// import { useEffect } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { logInAction } from './redux/actions/actionCreators/authActionCreators';
 
 const App: FC = () => {
-	// const dispatch = useDispatch();
-	// const history = useHistory();
-
-	// useEffect(() => {
-	// 	if (localStorage.getItem('token') && localStorage.getItem('userId')) {
-	// 		dispatch(logInAction(true));
-	// 		// history.push('/todos');
-	// 		// Redirect('/todos');
-	// 		console.log(history);
-	// 	}
-	// }, []);
-
 	return (
 		<BrowserRouter>
 			<NavBar />
